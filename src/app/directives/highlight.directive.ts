7e07dfecea8385d7005be121721c8b5667cf94d0
Kaspar Suvi <kaspar.suvi@iglu.ee>
2019-05-16 20:47:45 +0300

    Add uppercase directive tutorial

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,10 +1,14 @@
 import { Directive, ElementRef } from '@angular/core';
 
+// Directive decorator
 @Directive({
+  // Selector name to use in templates
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
+  // Defining ElementRef inside constructor parameters
   constructor(el: ElementRef) {
+    // Taket that element and add background color to it.
     el.nativeElement.style.backgroundColor = 'yellow';
   }
 }
---
+++ src/app/directives/tutorial.md
@@ -0,0 +1,8 @@
+# Uppercase directive
+Uppercase directive takes native element and makes its text uppercase
+
+1.  Open terminal and move to `/src/app/directives`
+2.  Generate uppercase directive
+> Tip: To generate directive use syntax: ng generate directive [name], u don't need any option <br/>
+> [Docs](https://angular.io/cli/generate#directive-command)
+3.
