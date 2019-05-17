13b308c3951c3cd78fa2d9d77a4b9c3211a64127
Kaspar Suvi <kaspar.suvi@iglu.ee>
2019-05-17 08:40:26 +0300

    Add news feature assignment

---
+++ src/assignment/4-news.md
@@ -0,0 +1,9 @@
+# Make your own feature module
+
+You will make news feed. Where you copy data from `4-news.ts` and add it to your container component(view). In the container component you should go throught news list and output presentational component about every news story. Output should have the news story card where title/date/author/tags/image is added.
+
+> Make it look nice! Maybe even use css? Its not hard just google it :)
+
+Try to make your own service, where you get all the news and maybe even you can add news?
+
+Looking for challenge? Try to add commenting feature!
---
+++ src/assignment/4-news.ts
@@ -0,0 +1,21 @@
+const news: INew[] = [
+  {
+    title: 'Angular Workshop',
+    text:
+      'Angular workshop was today! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce convallis id neque a luctus. Cras condimentum turpis eget dolor lacinia mattis. Duis libero justo, consectetur ac purus non, gravida malesuada nisi. Vestibulum fermentum, metus et dictum auctor, augue nunc venenatis nulla, nec maximus diam tellus ac quam. Etiam a eleifend sem. Vivamus efficitur eros odio, vel mollis elit efficitur id. Interdum et malesuada fames.',
+    author: 'Kaspar Suvi',
+    date: new Date(),
+    image: 'https://angular.io/assets/images/logos/angular/logo-nav@2x.png',
+    tags: ['Angular', 'workshop', 'iglu', 'TalTech']
+  },
+  {
+    title: 'New AI fake text generator may be too dangerous to release, say creators',
+    text:
+      'The creators of a revolutionary AI system that can write news stories and works of fiction – dubbed “deepfakes for text” – have taken the unusual step of not releasing their research publicly, for fear of potential misuse.',
+    author: 'Alex Hern',
+    date: new Date('02-04-2019'),
+    image:
+      'https://i.guim.co.uk/img/media/8aed182c9ed4504c6046ed00c322ea3492c93798/0_57_5106_3063/master/5106.jpg?width=620&quality=45&auto=format&fit=max&dpr=2&s=fcefc4a4026c9e8b1381b73f720213ae',
+    tags: ['AI', 'news', 'danferous']
+  }
+];
