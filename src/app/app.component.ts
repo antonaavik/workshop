583c3c623806367c1ea4af410927e2c6c4a522ce
Kaspar Suvi <kaspar.suvi@iglu.ee>
2019-05-17 09:44:06 +0300

    Remove today console.log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import {Component} from '@angular/core';
     <!--The content below is only a placeholder and can be replaced.-->
     <div style="text-align:center">
       <h1>Welcome to {{ title }}</h1>
-      <h1>Welcome to {{ today }}</h1>
     </div>
   `,
   styles: []
