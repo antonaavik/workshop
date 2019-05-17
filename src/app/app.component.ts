a0691987ffc1baca579846102df86281a60413f9
Kaspar Suvi <kaspar.suvi@iglu.ee>
2019-05-17 07:30:18 +0300

    Clear app.component"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,8 @@ import {Component} from '@angular/core';
   selector: 'app-root',
   template: `
     <!--The content below is only a placeholder and can be replaced.-->
-    <div *ngIf="!hide" style="text-align:center">
-      <p>{{ hero.name }}</p>
-      <p>{{ hero.birthday }}</p>
-      <p>{{ getShortcutName(hero) }}</p>
-      <button (click)="revive()">revive</button>
-      <input [value]="hero.name" (keyup)="updateHeroName($event)" />
-      <button (click)="hideInfo()">Someone watches me</button>
+    <div style="text-align:center">
+      <h1>Welcome to {{ title }}</h1>
     </div>
   `,
   styles: []
@@ -18,27 +13,10 @@ import {Component} from '@angular/core';
 export class AppComponent {
   title: string = 'Workshop';
   hero = {
-    name: 'Supermaan!',
-    birthday: new Date('1997-09-29')
+    name: 'Type-here-your-hero-name',
+    birthday: new Date()
   };
-  hide: boolean = false;
 
   // Use it to test your estday pipe
   today: Date = new Date();
-
-  getShortcutName(hero) {
-    return `${hero.name[0]}-${hero.birthday.getFullYear()}`;
-  }
-
-  revive() {
-    this.hero.birthday = this.today;
-  }
-
-  hideInfo() {
-    this.hide = true;
-  }
-
-  updateHeroName(e) {
-    this.hero.name = e.target.value;
-  }
 }
